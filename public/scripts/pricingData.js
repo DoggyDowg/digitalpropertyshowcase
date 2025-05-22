5de9bea5d49c8390e752727919f1568a91b038c9
DoggyDowg <rickmcolella@gmail.com>
2025-05-22 18:05:16 +0800

    Centralize pricing data into external JS file

---
+++ public/scripts/pricingData.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+const pricingData = {
+    essential: {
+        price: 'AU$460',
+        setupFee: '$0 setup fee'
+    },
+    premium: {
+        price: 'AU$520',
+        setupFee: '$220 setup fee'
+    },
+    prestige: {
+        price: 'AU$680',
+        setupFee: '$220 setup fee'
+    }
+}; 
